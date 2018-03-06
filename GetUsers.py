1e3b50f1a9083e1243fa24085121b3521a515dd8
Andy Fry <andy.fry@nec.com.au>
2018-03-06 03:25:10 +0000

    Only get non Admin/Guest users

--- GetUsers.py
+++ GetUsers.py
@@ -9,16 +9,11 @@
 from pyzabbix import ZabbixAPI
 import sys
 import argparse
+
+#Added to ignore the SSL certificate warnings
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#Get Hostgroup and item key from command line
-#parser = argparse.ArgumentParser()
-#parser.add_argument('-g','--hostgroup', help="Zabbix Hostgroup Name", required=True)
-#parser.add_argument('-k','--itemkey', help="Item Key", required=True)
-#args = parser.parse_args()
-#hostgroup=args.hostgroup
-#itemkey=args.itemkey
 username='Admin'
 password='Psbk^l&K'
 zabbix_server='https://127.0.0.1/zabbix'
@@ -31,10 +26,10 @@ z.login(user=username, password=password)
 
 # Get Users
 for user in z.user.get(output="extend",
-                  search={ "alias": 'Admin', "alias": 'guest' },
+#                  search={ "surname": 'Administrator', "alias": 'guest' },
+                  search={ "alias": [ "guest", "Admin" ] },
                   excludeSearch='on',
-                  searchByAny='false',
                   sortfield='userid',
                   sortorder='ASC'):
-#    print(user['userid'],user['autologin'],user['autologout'],user['lang'])
-    print(user)
+    print(user['userid'],user['name'],user['surname'],user['alias'])
+#    print(user)
